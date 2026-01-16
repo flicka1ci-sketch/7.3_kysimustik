4ec5f915434ddfa33b8240e2b1b96efc8441759d
opilane <opilane@HP-CZC2349HTB>
2026-01-16 14:06:45 +0200

    дома доделай тварь

--- kysimustik_module.py
+++ kysimustik_module.py
@@ -1,1 +1,17 @@
+import random
+import os
 
+
+def andmete_lugemine_failidest(fail="kusimused_vastused.txt"):
+    küs_vas = {}
+
+    if not os.path.exists(fail):
+        return küs_vas
+
+    with open(fail, "r", encoding="utf-8") as f:
+        for rida in f:
+            if ":" in rida:
+                kysimus, vastus = rida.strip().split(":", 1)
+                küs_vas[kysimus] = vastus
+
+    return küs_vas
